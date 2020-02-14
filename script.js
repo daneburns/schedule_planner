c1f14576ecf8fa0a3d34e5c35c85dad67a88ab72
Dane Burns <dane@lizaburnsandassociates.com>
2020-02-14 05:34:56 -0700

    more functionality

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
+
+
 var saveBtn = $(".saveBtn");
 saveBtn.on("click", function(e) {
-  alert("clicked!");
+  alert("clicked!", this.saveBtn[i]);
+  
 });
-var currentHour = moment().hour();
 
-var getHours = [];
-var test = [];
+var textAreas = $('textarea')
+console.log(textAreas[0])
+
+textAreaClear()
+function textAreaClear(){
+  for (let i = 0; i < textAreas.length; i++) {
+    var textAreaNum = textAreas[i]
+    if(!textAreaNum.hasClass('saved')){
+    textAreas[i].value = ''
+    }
+  }
+}
+
+var currentHour = moment().hour();
+var hours = $('.hour')
+var hoursArray = [];
+console.log(hours[0])
+
+
+console.log(textAreas)
 
 for (let i = 0; i < 9; i++) {
-  test.push(parseInt($(".hour")[i].textContent));
-  var timeBlock = $('.timeblock')
-  console.log(timeBlock);
-  if ($(".hour")[i].textContent) > currentHour) {
-    $(timeBlock[i]).addClass("future");
-  } else if (test < currentHour) {
-    $(timeBlock[i]).addClass("past");
-  } else {
-    $(timeBlock[i]).addClass("present");
+  hoursArray.push(parseInt(hours[i].textContent))
+}
+
+
+
+  // if hours = current hour then assign .present
+  // else if hours < current assign class .past
+  // else if hours > current assign class .future to the timeblock in the same div as that hour 
+
+for (let i = 0; i < hoursArray.length; i++) {
+  if(hoursArray[i] > currentHour){
+    $(textAreas[i]).addClass('future')
   }
+  else if(hoursArray[i] < currentHour){
+    $(textAreas[i]).addClass('past')
+  }
+  else{
+    $(textAreas[i]).addClass('present')
+  }
+  
+}
+
+
